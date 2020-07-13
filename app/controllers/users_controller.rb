c4ff6fd4e72221bc1b8d07662f5e35bd2dec2436
thanhan1181999 <nguyenthanhan1181999@gmail.com>
2020-07-13 17:51:17 +0700

    sign up basic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,21 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
+  def create
+    @user = User.new(user_params) # Not the final implementation!
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'git 
+    end
+  end
+  def show
+    @user = User.find(params[:id])
+  end
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'users#new'
+  get '/signup' , to: 'users#new'
+  post '/signup', to: 'users#create'
+  resources :users
 end
